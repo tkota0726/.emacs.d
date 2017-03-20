39faadae340da92c7399014a7ec2ad93e26d9d61
Kota <kota.taharaguchi@cellid.com>
2017-03-20 15:22:39 +0900

    マージ前コミット

--- init.el
+++ init.el
@@ -174,20 +174,22 @@
 (global-set-key [(meta f3)] 'highlight-symbol-query-replace)
 
 
-;;swoop設定
 (require 'swoop)
 (global-set-key (kbd "C-o")   'swoop)
 (global-set-key (kbd "C-M-o") 'swoop-multi)
 (global-set-key (kbd "M-o")   'swoop-pcre-regexp)
 (global-set-key (kbd "C-S-o") 'swoop-back-to-last-position)
 (global-set-key (kbd "H-6")   'swoop-migemo) ;; Option for Japanese match
-
 ;; 検索の移行
 ;; isearch     > press [C-o] > swoop
 ;; evil-search > press [C-o] > swoop
 ;; swoop       > press [C-o] > swoop-multi
-(define-key isearch-mode-map (kbd "C-o") 'swoop-from-search)
-(define-key evil-motion-state-map (kbd "C-o") 'swoop-from-evil-search)
+(define-key isearch-mode-map (kbd "C-o") 'swoop-from-isearch)
+;; (define-key evil-motion-state-map (kbd "C-o") 'swoop-from-evil-search)
 (define-key swoop-map (kbd "C-o") 'swoop-multi-from-swoop)
 ;; "yes or no" の選択を "y or n" にする
 (fset 'yes-or-no-p 'y-or-n-p)
+;;html補完機能用
+(add-hook 'html-helper-load-hook '(lambda () (require 'html-font)))
+(autoload 'html-helper-mode "html-helper-mode" "Yay HTML" t)
+(setq auto-mode-alist (cons '("\\html?$" . html-helper-mode) auto-mode-alist))
